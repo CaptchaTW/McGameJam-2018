7c256588853bb2be65d0fa634c88b33424c40dfc
CaptchaTW <32964205+CaptchaTW@users.noreply.github.com>
2018-03-24 10:43:38 -0400

    sound files
    test version

---
+++ sound.lua
@@ -0,0 +1,10 @@
+music = love.audio.newSource("cute2.mp3")
+music1 = love.audio.newSource("poop2.mp3")
+
+--music:play()
+
+--music:setLooping(true)--need actual loop condition for new boss appears or smthing
+music:setPitch(love.math.random()) --for sound effects
+
+music1:play()
+music:setLooping(true)
