4eeb9c932a8cc1fcf014110dc0d58187fb4c5651
axolotllotl <33985463+axolotllotl@users.noreply.github.com>
2018-03-25 12:43:02 -0400

    new thing

--- projectile.lua
+++ projectile.lua
@@ -11,6 +11,8 @@ local lazeranim = anim8.newAnimation(grid(1, '1-4'), 0.1)
 local vineimg = love.graphics.newImage('sprites/fourthformvine.png')
 local vinegrid = anim8.newGrid(32, 128, vineimg:getWidth(), vineimg:getHeight())
  
+local img = love.graphics.newImage('sprites/secondformimpactwave.png')
+local grid = anim8.newGrid(16, 16, img:getWidth(), img:getHeight())
 
 local Projectile = class ("Projectile", Entity)
 
@@ -37,6 +39,12 @@ function Projectile:initialize( world, x, y, w, h, dx, dy, type)
 		self.timer:after(0.8, function() self.damaging = false self.passable = false end)
 		self.timer:after(1.1, function() self:destroy() self.dying = true end)
 	else
+		self.anim = anim8.newAnimation(grid('1-5', 1), 0.1, 'pauseAtEnd')
+		if self.dx > 0 then 
+			self.Sx = 1 
+		else
+			self.Sx = -1 
+		end
   	self.timer:after(10, function() self:destroy() self.dying = true end)
   end
 end
@@ -77,7 +85,7 @@ function Projectile:draw(debug)
 	elseif self.type == 'vine' then
 		self.anim:draw(vineimg, self.x-18, self.y)
 	else
-		love.graphics.rectangle('fill', self.x, self.y, self.w, self.h)
+		self.anim:draw(img, self.x, self.y, 0, self.Sx, 1)
 	end
 
 end
